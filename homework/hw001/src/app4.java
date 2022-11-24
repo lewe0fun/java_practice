5be4e79cde41868933f12c3f92dfb6700734e624
lewe0fun <pakuln.yury@gmail.com>
2022-11-25 02:51:25 +0300

    Update app4.java

--- homework/hw001/src/app4.java
+++ homework/hw001/src/app4.java
@@ -2,7 +2,7 @@
 public class app4 {
     public static boolean check(String digit1,String qInt)
     {
-        if(!(digit1.length()==qInt.length()))return false;
+        if(digit1.length()==qInt.length()) {
         int counterXValue=0;
         int counterChecks=0;
         for (int i=0;i<digit1.length();i++)
@@ -13,38 +13,31 @@ public class app4 {
                     counterChecks++;
         if((digit1.length()-counterXValue)==counterChecks)
             return true;
+        }
         return false;
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 //        String input = "3?1+?4?=1167";
         String input = "??1+?4?=1167";
 //        String input = "???1+?4?=1167";
 //        String input = "2?36+6?=2601";
 //        String input="2?+?5=69";
 //        String input ="2?4+9?=355";
-        String digit1="",digit2="",result="",digit1max="",digit2max="";
-        int plusPos=0, equalPos=0,qInt=0,wInt=0,eInt=0,counter=0;
-        for(int i=0; i<input.length();i++)
-        {
+        String digit1="",digit2="",result="";
+        int plusPos=0, equalPos=0,counter=0;
+        for(int i=0; i<input.length();i++) {
             if(input.charAt(i)=='+')plusPos=i;
             if(input.charAt(i)=='=')equalPos=i;
         }
         digit1=input.substring(0,plusPos);
         digit2=input.substring(plusPos+1,equalPos);
         result=input.substring(equalPos+1);
-        digit1max=digit1.replace('?','9');
-        digit2max=digit2.replace('?','9');
         System.out.printf("У уровнения типа: %s+%s=%s\n",digit1,digit2,result);
-        qInt=Integer.parseInt(digit1max);
-        wInt=Integer.parseInt(digit2max);
-        eInt=Integer.parseInt(result);
-        for (int i=0;i<=qInt;i++)
-            for (int j =0; j<=wInt;j++)
-                if(i+j==eInt)
-                    if(check(digit1,String.valueOf(i)) && check(digit2,String.valueOf(j)))
-                    {
-                        System.out.printf("%d+%d=%d\n",i,j,eInt);counter++;
+        for (int i=0;i<=Integer.parseInt(digit1.replace('?','9'));i++)
+            for (int j =0; j<=Integer.parseInt(digit2.replace('?','9'));j++)
+                if(i+j==Integer.parseInt(result))
+                    if(check(digit1,String.valueOf(i)) && check(digit2,String.valueOf(j))) {
+                        System.out.printf("%d+%d=%d\n",i,j,Integer.parseInt(result));counter++;
                     }
         if (counter==0) System.out.println("Нет решений");
         else System.out.printf("Решений %d",counter);
